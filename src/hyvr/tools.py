ecf969e4c78647fb16d729884d776a83dd5d2b19
vcantarella <vcantarella@gmail.com>
2024-03-22 16:51:38 +0100

    minor fixes to surface regression

--- src/hyvr/tools.py
+++ src/hyvr/tools.py
@@ -91,7 +91,14 @@ def contact_surface(x:np.array,y:np.array,
     '''
     return None
 
-def surface_gauss_regression(x:np.array,y:np.array,mean:np.float64,variance:np.float64,corl:np.array,dataset:np.array, error:np.array):
+def surface_gauss_regression(
+    x:np.array,
+    y:np.array,
+    mean:np.float64,
+    variance:np.float64,
+    corl:np.array,
+    dataset:np.array,
+    error:np.array):
     '''
     Performs surface gaussian regression on input x,y data with given dataset (x,y,z) and error
     Based on the algorithm from Rasmussen & Williams (2006) Gaussian Processes for Machine Learning
@@ -104,41 +111,42 @@ def surface_gauss_regression(x:np.array,y:np.array,mean:np.float64,variance:np.f
     # Calculating distances:
     shape = x.shape
 
-    x = x.ravel()
-    y = y.ravel()
-    x_dist = pdist(x, 'sqeuclidean')
-    y_dist = pdist(y, 'sqeuclidean')
+    x = np.expand_dims(x.ravel(), axis=1)
+    y = np.expand_dims(y.ravel(),axis=1)
 
     # distance of the training points:
-    x_t = dataset[:,0].ravel()
-    y_t = dataset[:,1].ravel()
+    x_t = np.expand_dims(dataset[:,0].ravel(),axis=1)
+    y_t = np.expand_dims(dataset[:,1].ravel(),axis=1)
     z_t = dataset[:,2].ravel()
-    x_dist_t = pdist(x_t, 'sqeuclidean')
-    y_dist_t = pdist(y_t, 'sqeuclidean')
+    x_dist_t = pdist(x_t, 'euclidean')
+    y_dist_t = pdist(y_t, 'euclidean')
 
     
     #covariance matrix:
     @numba.njit()
     def kernel_2d(sigma_f, M, x_dist, y_dist)->np.float64:
         x = np.expand_dims(np.array((x_dist, y_dist)), axis=1)
-        return sigma_f*np.exp(-(1/2) * x.T @ M @ x)
+        x = x.astype(np.float64)
+        cov = sigma_f*np.exp(-(1/2) * x.T @ M @ x)
+        return cov[0,0]
     
     M = np.eye(2)*np.array([1/corl[0]**2,1/corl[1]**2])
 
     @numba.njit()
     def cov_matrix(sigma_f,x_dist,y_dist,M):
-        cov = np.zeros(x_dist.shape)
+        cov = np.zeros(x_dist.shape[0])
         for i in range(x_dist.shape[0]):
             cov[i] = kernel_2d(sigma_f,M,x_dist[i],y_dist[i])
         return cov
     
     cov = cov_matrix(variance,x_dist_t,y_dist_t,M)
     cov = squareform(cov)
+    cov = cov + np.eye(cov.shape[0])
     error = np.diag(error)
     cov = cov + error
     # Cholesky decomposition
-    L = scipy.linalg.cholesky(cov, lower=True)
-    L_y = scipy.linalg.solve_triangular(L, z_t, lower=True)
+    L, lower = scipy.linalg.cho_factor(cov, lower=True)
+    L_y = scipy.linalg.cho_solve((L, lower), z_t)
 
     #covariance between test and training points:
     @numba.njit()
@@ -146,8 +154,8 @@ def surface_gauss_regression(x:np.array,y:np.array,mean:np.float64,variance:np.f
         x_dist = np.zeros((x.shape[0],x_t.shape[0]))
         y_dist = np.zeros((x.shape[0],x_t.shape[0]))
         for i in range(x.shape[0]):
-            x_dist[i,:] = (x[i]-x_t)**2
-            y_dist[i,:] = (y[i]-y_t)**2
+            x_dist[i,:] = np.ravel((x[i,0]-x_t)**2)
+            y_dist[i,:] = np.ravel((y[i,0]-y_t)**2)
         return x_dist,y_dist
     
     x_dist,y_dist = distance(x,y,x_t,y_t)
--- tests/test_surface.py
+++ tests/test_surface.py
@@ -1,22 +1,46 @@
 import numpy as np
-from hyvyr.tools import surface_gauss_regression, ferguson_curve
+from hyvr.tools import surface_gauss_regression, ferguson_curve
 
 # Test data
+def test_curve():
+    h=0.1,
+    k=np.pi / 100,
+    eps_factor=1,
+    channel_curve_1 = ferguson_curve(
+    h=0.1,
+    k=np.pi / 200,
+    eps_factor=(np.pi / 1.5) ** 2,
+    flow_angle=0.0,
+    s_max=400,
+    xstart=0,
+    ystart=25,
+    )
+    x_t, y_t, vx, vy, s = channel_curve_1
+
+
 def test_gauss_surface_regression():
     x = np.linspace(0,100, 1000)
     y = np.linspace(0,100, 1000)
     x, y = np.meshgrid(x, y)
     # training data:
+    h=0.4,
+    k=np.pi / 200,
+    eps_factor=(np.pi / 1.5) ** 2,
+    x_t, y_t, vx, vy, s = ferguson_curve(
     h=0.1,
     k=np.pi / 200,
     eps_factor=(np.pi / 1.5) ** 2,
-    x_t, y_t, vx, vy, s = ferguson_curve(h, k, eps_factor, 0, 200, 50, 50)
+    flow_angle=0.0,
+    s_max=400,
+    xstart=0,
+    ystart=25,
+    )
     z_t = np.sin(s)
     dataset = np.array([x_t, y_t, z_t]).T
-    error = np.random.normal(0, 0.1, len(x_t))
+    error = np.repeat(0.3, dataset.shape[0])
     corl = np.array([10, 10])
-    mean = 0
-    variance = 1
+    mean = 0.
+    variance = 1.
     Z = surface_gauss_regression(x, y, mean, variance, corl, dataset, error)
     assert Z.shape == x.shape
     assert Z.shape == y.shape
