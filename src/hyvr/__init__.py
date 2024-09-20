8af5683b8567f03693c5aa86c807c7905394e428
vcantarella <vcantarella@gmail.com>
2024-09-20 14:27:07 +0200

    correcting ruff errors

--- src/hyvr/__init__.py
+++ src/hyvr/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1.0b"
-from .objects.trough import half_ellipsoid
-from .objects.sheet import sheet
-from .objects.channel import channel
+from .objects.trough import half_ellipsoid as half_ellipsoid
+from .objects.sheet import sheet as sheet
+from .objects.channel import channel as channel
